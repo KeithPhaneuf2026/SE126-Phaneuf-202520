fe7727116a27ff6687dbf0691b496bfe13c5db4b
KeithPhaneuf2026 <kmphaneuf@email.neit.edu>
2025-01-24 11:20:47 -0500

    finished in class lab
    finished in class lab

--- Week 3/In class lab/inclass.py
+++ Week 3/In class lab/inclass.py
@@ -4,9 +4,7 @@
 #1-23-2025 [W3D2]
 
 #PROGRAM PROMPT:  
-# You have been asked to produce a report that lists all the computers in the csv file filehandling.csv. 
-# Your report should look like the following sample output.  
-# The last line should print the number of computers in the file.
+#Your CIO (Chief Information Officer) has asked you to determine how much it would cost the company to replace all machines that are from 2016 and earlier. He plans on spending not more than $2,000 dollars for desktops and $1,500 for laptops.  Store the data from the file lab3a.csv into lists.  Then process the lists to reprint all of the file information (exactly as you did in Lab 2) and also produce an end report that lists the number of desktops that will be replaced, the cost to replace the desktops, the number of laptops that will be replaced, and the cost to replace the laptops.
 
 
 #VARIABLE DICTIONARY
@@ -20,6 +18,8 @@
 #second_disk    if the computer has an extra drive
 #os             operating system
 #yr             year computer was made
+#old_desktop    counts how many desktops are 2016 and older and need to be replaced
+#old_laptop    counts how many laptops are 2016 and older and need to be replaced
 
 #Imports
 import csv
@@ -37,9 +37,10 @@ no_hdd = []
 second_disk = []
 os = []
 yr = []
+#prints heading
 print(f"\n\n{'Type':20}     {'Brand':5}               {'CPU':5}   {'RAM':5}   {'1st Disk':5}    {'No HDD':5}      {'2nd Disk':5}   {'OS':5}     {'YR':5}") #header
 print("-------------------------------------------------------------------------------------------------------------------------------")
-with open("Week 3/In class lab/filehandling-1.csv") as csvfile: 
+with open("Week 3/In class lab/filehandling-1.csv") as csvfile: #file
     file = csv.reader(csvfile)
     for rec in file:
 
@@ -48,12 +49,13 @@ with open("Week 3/In class lab/filehandling-1.csv") as csvfile:
             rec[0] = "Laptop"
         else:
             rec[0] = "Desktop"
-        if (brand == "DL"):#converts values to be more readable
-            brand.append("Dell")
-        elif (brand == "HP"):#converts values to be more readable
-            brand.append("Hewlett Packard")
-        elif (brand == "GW"):#converts values to be more readable
-            brand.append("Gateway")
+        if rec[1] == "DL": #converts values to be more readable
+            rec[1] = "Dell"
+        elif rec[1] == "HP":#converts values to be more readable
+            rec[1] = "Hewlett Packard"
+        elif rec[1] == "GW":#converts values to be more readable
+            rec[1] = "Gateway"
+        #append statements add data to the empty lists
         type.append(rec[0])
         brand.append(rec[1])
         cpu.append(rec[2])
@@ -73,4 +75,20 @@ for index in range(0, len(type)):
     #for every item, index will start at 0 and run up to (not including) the length (# of items)
  
     print(f"{type[index]:20}     {brand[index]:18}  {cpu[index]:5}   {ram[index]:5}   {first_disk[index]:5}   {no_hdd[index]:5}           {second_disk[index]:5}      {os[index]:5}     {yr[index]:5}") #displays data
-print(f"Total Computers in the file: {total_rec}") #displays total amount of computers
+
+#counters for computers that need to be replaced
+old_desktop = 0
+old_laptop = 0
+
+for i in range(0, len(yr)):
+    #counts how many desktops and laptops need to be replaced
+
+    if int(yr[i]) <= 16: #too outdated
+        if type[i] == "Desktop":
+            old_desktop += 1
+        else:
+            old_laptop += 1 #if not a desktop, it is a laptop and the laptop counter increases
+#print statements below display how many computers need to be replaced, the cost of replacement, and the total amount of computers in the file. 
+print(f"\nTotal amount of desktops that need to be replaced: {old_desktop} Cost: ${old_desktop * 2000:.2f}")
+print(f"\nTotal amount of laptops that need to be replaced: {old_laptop} Cost: ${old_laptop * 1500:.2f}")
+print(f"\nTotal Computers in the file: {total_rec}") #displays total amount of computers
