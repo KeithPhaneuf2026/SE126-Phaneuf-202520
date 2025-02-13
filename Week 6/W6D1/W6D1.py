dda8abb63eb81f3ae766942a80c80b8498214bed
KeithPhaneuf2026 <kmphaneuf@email.neit.edu>
2025-02-13 17:30:45 -0500

    w6d2
    w6d2

---
+++ Week 6/W6D1/W6D1.py
@@ -0,0 +1,12 @@
+#Keith Phaneuf
+#SE126.04
+#in class lab
+#2-13-2025 [W6D2]
+
+#PROGRAM PROMPT:
+#Binary Search
+#separating a collection into two halves (low/high)
+#can only find one unique value
+#can only be performed on ordered collections
+#ascending:  low --> high
+#descending: low <-- high
